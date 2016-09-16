f364e914c98272c333a89473d5bebfd45e2fd89c
phoe-nix <wanwei1116@sina.com>
2016-09-16 15:34:51 +0800

    added Chinese custom font

--- blit.c
+++ blit.c
@@ -10,10 +10,12 @@
 #include <string.h>
 
 #include "blit.h"
+#include "font_chn.c"
 
 #define ALPHA_BLEND 1
+#define FONTX2 1
 
-extern unsigned char msx[];
+//extern unsigned char msx[];
 
 /////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
@@ -79,12 +81,13 @@ void blit_set_color(int fg_col,int bg_col)
 /////////////////////////////////////////////////////////////////////////////
 // blit text
 /////////////////////////////////////////////////////////////////////////////
+
 int blit_string(int sx,int sy,const char *msg)
 {
 	int x,y,p;
-	int offset;
-	char code;
-	unsigned char font;
+	int offset,font_offset;
+	char code1,code2;
+	unsigned char *font;
 	uint32_t fg_col,bg_col;
 
 #if ALPHA_BLEND
@@ -101,52 +104,130 @@ int blit_string(int sx,int sy,const char *msg)
 //Kprintf("MODE %d WIDTH %d\n",pixelformat,bufferwidth);
 	if( (bufferwidth==0) || (pixelformat!=0)) return -1;
 
-	for(x=0;msg[x] && x<(pwidth/16);x++)
+	x=0;
+	while(msg[x] && x<(pwidth/16))
 	{
-		code = msg[x] & 0x7f; // 7bit ANK
-		for(y=0;y<8;y++)
+		code1 = msg[x];
+		
+		if (code1 < 128)
 		{
-			offset = (sy+(y*2))*bufferwidth + sx+x*16;
-			font = y>=7 ? 0x00 : msx[ code*8 + y ];
-			for(p=0;p<8;p++)
+			code2 = 0;
+			font = &msx_asc[code1 * 12];
+			for(y=0;y<12;y++,font++)
 			{
+				for(p=0;p<6;p++)
+				{
+#if FONTX2
+					offset = (sy+(y*2))*bufferwidth + sx+x*12+p*2;
+#else
+					offset = (sy+y)*bufferwidth + sx+x*6+p;
+#endif
+
 #if ALPHA_BLEND
-				col = (font & 0x80) ? fg_col : bg_col;
-				alpha = col>>24;
-				if(alpha==0)
-				{
-					vram32[offset] = col;
-					vram32[offset + 1] = col;
-					vram32[offset + bufferwidth] = col;
-					vram32[offset + bufferwidth + 1] = col;
-				}
-				else if(alpha!=0xff)
-				{
-					c2 = vram32[offset];
-					c1 = c2 & 0x00ff00ff;
-					c2 = c2 & 0x0000ff00;
-					c1 = ((c1*alpha)>>8)&0x00ff00ff;
-					c2 = ((c2*alpha)>>8)&0x0000ff00;
-					uint32_t color = (col&0xffffff) + c1 + c2;
+					col = (*font & (0x80 >> p)) ? fg_col : bg_col;
+					alpha = col>>24;
+					if(alpha==0)
+					{
+						vram32[offset] = col;
+						vram32[offset + 1] = col;
+						vram32[offset + bufferwidth] = col;
+						vram32[offset + bufferwidth + 1] = col;
+					}
+					else if(alpha!=0xff)
+					{
+						c2 = vram32[offset];
+						c1 = c2 & 0x00ff00ff;
+						c2 = c2 & 0x0000ff00;
+						c1 = ((c1*alpha)>>8)&0x00ff00ff;
+						c2 = ((c2*alpha)>>8)&0x0000ff00;
+						uint32_t color = (col&0xffffff) + c1 + c2;
+						vram32[offset] = color;
+						vram32[offset + 1] = color;
+						vram32[offset + bufferwidth] = color;
+						vram32[offset + bufferwidth + 1] = color;
+					}
+#else
+					uint32_t color = (*font & 0x80) ? fg_col : bg_col;
 					vram32[offset] = color;
 					vram32[offset + 1] = color;
 					vram32[offset + bufferwidth] = color;
 					vram32[offset + bufferwidth + 1] = color;
-				}
-#else
-				uint32_t color = (font & 0x80) ? fg_col : bg_col;
-				vram32[offset] = color;
-				vram32[offset + 1] = color;
-				vram32[offset + bufferwidth] = color;
-				vram32[offset + bufferwidth + 1] = color;
 #endif
-				font <<= 1;
-				offset+=2;
+				}
 			}
+			x++;
+		}
+		else
+		{
+			code2 = msg[x+1];
+
+			{
+				if( (code1 >= 0xA1) && (code1 <= 0xFE) && (code2 >= 0xA1) && (code2 <= 0xFE) )
+					font_offset = 24 + ((94*(code1-0xA1)+(code2-0xA1))*24);
+				else
+					font_offset = 0;
+
+				if(font_offset+24 > sizeof(msx_chs))
+					font_offset = 0;
+
+				font = &msx_chs[ font_offset ];
+			}
+
+			for(y=0;y<12;y++,font+=2)
+			{
+				for(p=0;p<12;p++)
+				{
+#if ALPHA_BLEND
+					if (p < 8)
+						col = (*font & (128 >> p)) ? fg_col : bg_col;
+					else
+						col = (*(font+1) & (128 >> (p-8))) ? fg_col : bg_col;
+
+					alpha = col>>24;
+#if FONTX2
+					offset = (sy+(y*2))*bufferwidth + sx+x*12+p*2;
+#else
+					offset = (sy+y)*bufferwidth + sx+x*6+p;
+#endif
+					if(alpha==0)
+					{
+						vram32[offset] = col;
+						vram32[offset + 1] = col;
+						vram32[offset + bufferwidth] = col;
+						vram32[offset + bufferwidth + 1] = col;
+					}
+					else if(alpha!=0xff)
+					{
+						c2 = vram32[offset];
+						c1 = c2 & 0x00ff00ff;
+						c2 = c2 & 0x0000ff00;
+						c1 = ((c1*alpha)>>8)&0x00ff00ff;
+						c2 = ((c2*alpha)>>8)&0x0000ff00;
+						uint32_t color = (col&0xffffff) + c1 + c2;
+						vram32[offset] = color;
+						vram32[offset + 1] = color;
+						vram32[offset + bufferwidth] = color;
+						vram32[offset + bufferwidth + 1] = color;
+					}
+#else
+				
+					if (p < 8)
+					uint32_t color = (*(font+1) & 0x80 >> p)) ? fg_col : bg_col;
+					else
+					uint32_t color = (*(font+1) & 0x80 >> (p-8))) ? fg_col : bg_col;
+					vram32[offset] = color;
+					vram32[offset + 1] = color;
+					vram32[offset + bufferwidth] = color;
+					vram32[offset + bufferwidth + 1] = color;
+#endif
+				}
+			}
+			x+=2;
 		}
 	}
 	return x;
 }
+
 
 int blit_string_ctr(int sy,const char *msg)
 {
